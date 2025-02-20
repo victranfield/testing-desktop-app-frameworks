a8a674f83d86025458c9421066f5503301a77b25
victranfield <victoria.tranfield@gmail.com>
2025-02-20 16:37:45 +0000

    renderer updated with dark mode

--- renderer.js
+++ renderer.js
@@ -4,3 +4,14 @@ const CLICK_MESSAGE = 'Notification clicked!'
 
 new window.Notification(NOTIFICATION_TITLE, { body: NOTIFICATION_BODY })
   .onclick = () => { document.getElementById('output').innerText = CLICK_MESSAGE }
+
+
+  document.getElementById('toggle-dark-mode').addEventListener('click', async () => {
+    const isDarkMode = await window.darkMode.toggle()
+    document.getElementById('theme-source').innerHTML = isDarkMode ? 'Dark' : 'Light'
+  })
+  
+  document.getElementById('reset-to-system').addEventListener('click', async () => {
+    await window.darkMode.system()
+    document.getElementById('theme-source').innerHTML = 'System'
+  })
