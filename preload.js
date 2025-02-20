feec67c4b9076ef60f38c3097713d4fd1165836c
victranfield <victoria.tranfield@gmail.com>
2025-02-20 16:04:18 +0000

    dark mode toggle added

--- preload.js
+++ preload.js
@@ -1,3 +1,15 @@
+// preload.js
+
+// All the Node.js APIs are available in the preload process.
+// It has the same sandbox as a Chrome extension.
+
+const { contextBridge, ipcRenderer } = require('electron/renderer')
+
+contextBridge.exposeInMainWorld('darkMode', {
+  toggle: () => ipcRenderer.invoke('dark-mode:toggle'),
+  system: () => ipcRenderer.invoke('dark-mode:system')
+})
+
 window.addEventListener('DOMContentLoaded', () => {
     const replaceText = (selector, text) => {
       const element = document.getElementById(selector)
