35ff8cbed1cf52b7a30086f28c64f3d9ad26e343
victranfield <victoria.tranfield@gmail.com>
2025-02-20 15:47:59 +0000

    function showNotification added

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
+// main.js
+
+// Modules to control application life and create native browser window
 const { app, BrowserWindow } = require('electron')
 const path = require('node:path')
 
-const createWindow = () => {
+function createWindow() {
+    // Create the browser window.
   const win = new BrowserWindow({
     width: 800,
     height: 600,
@@ -10,12 +14,25 @@ const createWindow = () => {
     }
   })
 
-  win.loadFile('index.html')
+   // and load the index.html of the app.
+   win.loadFile('index.html')
 }
 
+const NOTIFICATION_TITLE = 'Basic Notification'
+const NOTIFICATION_BODY = 'Notification from the Main process'
+
+function showNotification () {
+  new Notification({ title: NOTIFICATION_TITLE, body: NOTIFICATION_BODY }).show()
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
   createWindow()
+  .then(showNotification)
 })
+
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') app.quit()
@@ -25,6 +42,9 @@ app.whenReady().then(() => {
   createWindow()
 
   app.on('activate', () => {
+
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
   })
 })
